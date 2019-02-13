bc39d30bf1f3ac1fb349a90936cc397f32d5649b
TheTides <luke.milby@gmail.com>
2019-02-13 09:11:13 -0600

    refactor test

--- strucmethinter/shapes_test.go
+++ strucmethinter/shapes_test.go
@@ -17,18 +17,21 @@ func TestPerimeter(t *testing.T){
 
 func TestArea(t *testing.T){
 	areaTests := []struct{
+		name string
 		shape Shape
-		want float64
+		hasArea float64
 	}{
-		{Rectangle{12,6}, 72},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12,6}, 36},
+		{"Rectangle", Rectangle{12,6}, 72},
+		{"Circle", Circle{10}, 314.1592653589793},
+		{"Triangle", Triangle{12,6}, 36},
 	}
 
 	for _, tt := range areaTests{
-		got := tt.shape.Area()
-		if got != tt.want{
-			t.Errorf("got %.2f want %.2f", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea{
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
