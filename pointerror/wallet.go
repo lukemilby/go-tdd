5d7630ab51eb0fb7324e514e52daffb698e63613
TheTides <luke.milby@gmail.com>
2019-02-15 08:44:55 -0600

    added error

--- pointerror/wallet.go
+++ pointerror/wallet.go
@@ -1,16 +1,35 @@
 package pointerror
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+)
+
+type Stringer interface {
+	String() string
+}
+type Bitcoin int
 
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func(w *Wallet) Deposit(amount int){
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
-	w.balance += amount
+func(w *Wallet) Deposit(amount Bitcoin){
+	w.balance += Bitcoin(amount)
 }
 
-func(w *Wallet) Balance() int{
+func(w *Wallet) Withdraw(amount Bitcoin) error{
+	if amount > w.balance {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
+	w.balance -= Bitcoin(amount)
+	return nil
+}
+
+func(w *Wallet) Balance() Bitcoin{
 	return w.balance
+}
+
+func (b Bitcoin) String() string{
+	return fmt.Sprintf("%d BTC", b)
 }
