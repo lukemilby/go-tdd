eb5ad1d6dff46f56493705344d8fad618ca77bd8
TheTides <luke.milby@gmail.com>
2019-02-16 15:47:32 -0600

    added tests and Search method

---
+++ maps/dictionary_test.go
@@ -0,0 +1,36 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T){
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word",func(t *testing.T){
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("Unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func assertStrings(t *testing.T, got, want string){
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got '%s' want '%s' given", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error){
+	t.Helper()
+
+	if got != want{
+		t.Errorf("got error '%s' want '%s'", got, want)
+	}
+}
