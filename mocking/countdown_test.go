bdbdd9836f6d5cb4e38b93ef561d94398b5f425f
TheTides <luke.milby@gmail.com>
2019-02-17 17:38:21 -0600

    added Countdown operation spy

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -34,3 +34,21 @@ type SpySleeper struct{
 func (s *SpySleeper) Sleep(){
 	s.Calls++
 }
+
+type CountdownOperationSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationSpy) Write(p []byte) (n int err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const (
+	write = "write"
+	sleep = "sleep"
+)
