2ea8e0b415073e46439c7e337031a1d652bf503a
TheTides <luke.milby@gmail.com>
2019-02-23 18:25:56 -0600

    refactor

--- selects/racer.go
+++ selects/racer.go
@@ -1,1 +1,22 @@
 package selects
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string){
+	aDuration := mesureResponseTime(a)
+	bDuration := mesureResponseTime(b)
+
+	if aDuration < bDuration {
+		return a
+	}
+	return b
+}
+
+func mesureResponseTime(url string) time.Duration{
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
