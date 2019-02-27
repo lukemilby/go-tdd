2a66e096cc3985ae0796a12edf742ac24e49aefb
TheTides <luke.milby@gmail.com>
2019-02-27 07:44:47 -0600

    added new case, refactor walk

--- reflections/reflection.go
+++ reflections/reflection.go
@@ -1,5 +1,12 @@
 package reflections
 
-func walk(x interface{}, fn func(input string)){
+import "reflect"
 
+func walk(x interface{}, fn func(input string)){
+	val := reflect.ValueOf(x)
+
+	for i :=0; i<val.NumField();i++{
+		field := val.Field(i)
+		fn(field.String())
+	}
 }
--- reflections/reflection_test.go
+++ reflections/reflection_test.go
@@ -1,10 +1,46 @@
 package reflections
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestWalk(t *testing.T) {
+
+	cases := []struct{
+		Name string
+		Input interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with on string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+	}
+
+
+	for _, test := range cases{
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string){
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls){
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
 	expected := "Chris"
 	var got [] string
 
@@ -17,5 +53,8 @@ func TestWalk(t *testing.T) {
 	})
 	if len(got) != 1 {
 		t.Errorf("wrong number of fuction calls, got %d want %d", len(got), 1)
+	}
+	if got[0] != expected {
+		t.Errorf("got '%s', want '%s'", got[0], expected)
 	}
 }
